5b38757546614415d9b77b524277d8883aa8029a
Lucas Inocencio Pires <piresilucas@gmail.com>
2020-07-30 22:46:13 -0300

    Update permissions route

--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -5,8 +5,8 @@ const auth = require('../lib/auth');
 
 router.get('/', auth.verifyLoggedUser, auth.verifyGroupUser(['user', 'admin']), controller.getAllContacts);
 router.get('/:id', auth.verifyLoggedUser, controller.getContactById);
-router.post('/', auth.verifyLoggedUser, auth.verifyGroupUser(['admin']),controller.addContact);
-router.put('/:id', controller.updateContact);
-router.delete('/:id', controller.deleteContact);
+router.post('/', auth.verifyLoggedUser, auth.verifyGroupUser(['admin']), controller.addContact);
+router.put('/:id', auth.verifyLoggedUser, auth.verifyGroupUser(['admin']), controller.updateContact);
+router.delete('/:id', auth.verifyLoggedUser, auth.verifyGroupUser(['admin']), controller.deleteContact);
 
 module.exports = router;
